e357aa0ace96a552e8fc9d1d21b7a6288284c379
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-17 17:18:41 +0530

    Added : Vowel

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -2,16 +2,22 @@ package BridgeLabz;
 import java.util.Scanner;
 import java.utility.Utility;
 public class BasicCorePrograms {
-	public static void main(String[] args) {
-	    int num;
-	    System.out.println("Enter an  number:");
-	    Scanner input = new Scanner(System.in);
-	    num = input.nextInt();
-	    if ( num % 2 == 0 )
-	        System.out.println("Entered number is even");
-	    else
-	        System.out.println("Entered number is odd");
-	  }
+	public static void main(String[ ] arg)
+	{
+	
+	System.out.println("Enter a character : ");
+	char ch;
+	Scanner sc = new Scanner(System.in);
+	  ch = sc.next().charAt(0);
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' )
+            System.out.println(ch + " is vowel");
+        else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+    		System.out.println("Entered character "+ch+" is Consonent");
+        else
+            System.out.println(ch + " is not a alphabet");
+
+    }
 }
 
 
