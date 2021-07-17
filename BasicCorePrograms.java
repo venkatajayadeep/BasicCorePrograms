a527056bbeaf7aec14e9789f71cf9f8c89447bc4
venkatajayadeep <jayadeep101097@gmail.com>
2021-07-17 17:13:22 +0530

    Added : EvenOrOdd

--- BasicCorePrograms.java
+++ BasicCorePrograms.java
@@ -2,20 +2,16 @@ package BridgeLabz;
 import java.util.Scanner;
 import java.utility.Utility;
 public class BasicCorePrograms {
-	public static void main(String args[]) {
-		int x,y,t;
-		   Scanner sc = new Scanner(System.in); 
-		System.out.println("Enter the value of X and Y");  
-	       x = sc.nextInt();  
-	       y = sc.nextInt();  
-	       System.out.println("before swapping numbers: "+x +"  "+ y); 
-	       t = x;  
-	       x = y;  
-	       y = t;  
-	       System.out.println("After swapping: "+x +"   " + y);  
-	       System.out.println( );  
-	    } 
-
+	public static void main(String[] args) {
+	    int num;
+	    System.out.println("Enter an  number:");
+	    Scanner input = new Scanner(System.in);
+	    num = input.nextInt();
+	    if ( num % 2 == 0 )
+	        System.out.println("Entered number is even");
+	    else
+	        System.out.println("Entered number is odd");
+	  }
 }
 
 
